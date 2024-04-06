0ce1ff340ee4bc7193e490d3fa8d754786517d51
Jesus Guerrero <jesusant.guerrero@gmail.com>
2024-04-05 20:07:32 -0400

    chore: complete migrations to pages and functions

--- public/_worker.js
+++ public/_worker.js
@@ -1,32 +1,17 @@
 import { Ai } from "@cloudflare/ai";
-export interface Env {
-	AI: any
-	ASSETS: Fetcher;
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
-}
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext) {
+	async fetch(request, env, ctx) {
 		const ai = new Ai(env.AI)
 		const { searchParams, pathname } = new URL(request.url);
 		const distance = `${searchParams.get('distance') ?? ''}`;
 		const minutes = `${searchParams.get('minutes') ?? ''}`;
 		const weeks = `${searchParams.get('weeks') ?? 4}`;
+		const isApi = pathname.startsWith('/api/');
 
-		if (pathname.startsWith('/api/') && distance && minutes) {
+		console.log(isApi);
+
+		if (isApi && distance && minutes) {
 			const response = await ai.run('@cf/meta/llama-2-7b-chat-int8', {
 				messages: [
 					{
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,14 @@
-import { defineWorkersConfig } from "@cloudflare/vitest-pool-workers/config";
+import { fileURLToPath } from 'node:url'
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
+import viteConfig from './vite.config'
 
-export default defineWorkersConfig({
-  test: {
-    poolOptions: {
-      workers: {
-        wrangler: { configPath: "./wrangler.toml" },
-      },
-    },
-  },
-});
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude, 'e2e/*'],
+      root: fileURLToPath(new URL('./', import.meta.url))
+    }
+  })
+)
