5b49ee9c924d7f687b5854bc414213362e5e0997
Jesus Guerrero <jesusant.guerrero@gmail.com>
2024-04-12 16:18:24 -0400

    fix: improve header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,21 +3,28 @@ import { config } from '@/config';
 </script>
 
 <template>
-   <div class="relative z-50 flex justify-between px-5 py-3 text-white bg-base items-center border-b border-base-lvl-2">
-        <div class="flex items-center space-x-2">
-            <h1 class="text-2xl font-bold text-primary">
-				<router-link to="/">
-					{{ config.appName }}
-				</router-link>
-            </h1>
-        </div>
-        <div class="relative flex mr-24 space-x-2">
+   <header class="relative z-50 flex items-center text-white border-b bg-base border-base-lvl-2">
+        <section class="flex items-center justify-between w-full px-5 py-3 mx-auto max-w-7xl">
+			<div class="flex items-center space-x-2">
+				<h1 class="text-2xl font-bold text-primary">
+					<router-link to="/">
+						{{ config.appName }}
+					</router-link>
+				</h1>
+			</div>
+			<div class="relative flex mr-24 space-x-2">
 
-        </div>
-        <router-link to="/saved-plans" class="overflow-hidden bg-base-lvl-2  rounded-md cursor-pointer right-5">
-           My Plans
-        </router-link>
-    </div>
+			</div>
+			<section>
+				<router-link to="/" class="px-5 py-2 overflow-hidden cursor-pointer rounded-l-md bg-base-lvl-2">
+				   Home
+				</router-link>
+				<router-link to="/saved-plans" class="px-5 py-2 overflow-hidden cursor-pointer rounded-r-md bg-base-lvl-2">
+				   My Plans
+				</router-link>
+			</section>
+		</section>
+    </header>
 </template>
 
 <style>
